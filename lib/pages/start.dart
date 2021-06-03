934cc83095e9df13e52ef169829ff2da9f3cd224
K-shir0 <kakishiro2190394@gmail.com>
2021-06-04 00:20:00 +0900

    feat: タイトル画面を作成

--- lib/pages/start.dart
+++ lib/pages/start.dart
@@ -4,9 +4,20 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ElevatedButton(
-        onPressed: () => Navigator.pushNamed(context, '/play'),
-        child: Text("test"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Eカード", style: const TextStyle(fontSize: 40),),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, '/play'),
+                child: Text("ゲームを始める"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
