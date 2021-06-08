4f476171bee30e9e3a3b426e4628b55085217c0b
K-shir0 <kakishiro2190394@gmail.com>
2021-06-08 09:14:26 +0900

    feat: NFCを読み込む設定が完成

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,20 +12,19 @@ enum GameMode {
 
 class HomePage extends HookWidget {
   final eCardProvider = StateNotifierProvider<ECardNotifier, ECard>(
-    (refs) => ECardNotifier(),
+        (refs) => ECardNotifier(),
   );
 
   final MethodChannel _methodChannel = MethodChannel("samples.flutter.dev/nfc");
 
-   Future<String> scanNfcTag() async {
+  Future<String> scanNfcTag() async {
     String _uid = "";
 
     try {
       // _uid = await _methodChannel.invokeMethod("getNfcTag");
-      await _methodChannel.invokeMethod("getNfcTag").then((value) {
-        print(value);
-      });
-      print(_uid);
+      final uid = await _methodChannel.invokeMethod("getNfcTag");
+
+      return uid.toString();
     } catch (e) {
       print(e);
     }
@@ -37,6 +36,19 @@ class HomePage extends HookWidget {
   final GameMode mode;
 
   HomePage({this.mode = GameMode.normal});
+
+  Map<String, CardType> nfcs = {
+    "04c6296a6f7180": CardType.citizen,
+    "04ba296a6f7180": CardType.citizen,
+    "04c2296a6f7180": CardType.citizen,
+    "04b6296a6f7180": CardType.citizen,
+    "04a1296a6f7180": CardType.citizen,
+    "04a9296a6f7180": CardType.citizen,
+    "04a5296a6f7180": CardType.citizen,
+    "04be296a6f7180": CardType.citizen,
+    "04ae296a6f7180": CardType.slave,
+    "04b2296a6f7180": CardType.emperor,
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -53,108 +65,125 @@ class HomePage extends HookWidget {
 
     return Scaffold(
         body: Stack(
-      children: [
-        if (provider.phase >= 2)
-          GestureDetector(
-            onTap: () {
-              context.read(eCardProvider.notifier).nextPhase();
-            },
-          ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(' '),
-            const Text(' '),
-            Text('mode: ${mode.toString().split(".")[1]}'),
-            Text('turn: ${provider.turn}'),
-            Text('phase: ${provider.phase}'),
-            Text('cardChosenByPlayer1: ${provider.cardChosenByPlayer1}'),
-            Text('cardChosenByPlayer2: ${provider.cardChosenByPlayer2}'),
-            Text('winFlag: ${provider.winFlag}'),
-            Text('isHandfulVisible: ${provider.isHandfulVisible.toString()}'),
-          ],
-        ),
-        Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              if (provider.phase >= 1)
-                RotatedBox(
-                    quarterTurns: 2,
-                    child: ECardWidget(
-                        cardType: provider.isCardFront
-                            ? provider
+            if (provider.phase >= 2)
+              GestureDetector(
+                onTap: () {
+                  context.read(eCardProvider.notifier).nextPhase();
+                },
+              ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(' '),
+                const Text(' '),
+                Text('mode: ${mode.toString().split(".")[1]}'),
+                Text('turn: ${provider.turn}'),
+                Text('phase: ${provider.phase}'),
+                Text('cardChosenByPlayer1: ${provider.cardChosenByPlayer1}'),
+                Text('cardChosenByPlayer2: ${provider.cardChosenByPlayer2}'),
+                Text('winFlag: ${provider.winFlag}'),
+                Text('isHandfulVisible: ${provider.isHandfulVisible
+                    .toString()}'),
+              ],
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  if (provider.phase >= 2)
+                    RotatedBox(
+                        quarterTurns: 2,
+                        child: ECardWidget(
+                            cardType: provider.isCardFront
+                                ? provider
                                 .player2Deck[provider.cardChosenByPlayer2!]
                                 .cardType
-                            : null,
-                        onTapHandler: () {}))
-              else
-                Container(),
-              // 1Pカード
-              if (provider.phase >= 2)
-                RotatedBox(
-                    quarterTurns: 0,
-                    child: ECardWidget(
-                        cardType: provider.isCardFront
-                            ? provider
+                                : null,
+                            onTapHandler: () {}))
+                  else
+                    Container(),
+                  // 1Pカード
+                  if (provider.phase >= 1)
+                    RotatedBox(
+                        quarterTurns: 0,
+                        child: ECardWidget(
+                            cardType: provider.isCardFront
+                                ? provider
                                 .player1Deck[provider.cardChosenByPlayer1!]
                                 .cardType
-                            : null,
-                        onTapHandler: () {}))
-              else
-                Container(),
-            ],
-          ),
-        ),
-        if (provider.isHandfulVisible)
-          Padding(
-            padding: const EdgeInsets.only(left: 32, right: 32),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                if (mode != GameMode.scan)
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      for (int i = 0; i < playerDeck.length; i++)
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8, right: 8),
-                          child: Container(
-                            transform:
+                                : null,
+                            onTapHandler: () {}))
+                  else
+                    Container(),
+                ],
+              ),
+            ),
+            if (provider.isHandfulVisible)
+              Padding(
+                padding: const EdgeInsets.only(left: 32, right: 32),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    if (mode != GameMode.scan)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          for (int i = 0; i < playerDeck.length; i++)
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8, right: 8),
+                              child: Container(
+                                transform:
                                 Matrix4.translationValues(0.0, 54.0, 0.0),
-                            child: ECardWidget(
-                              cardType: playerDeck[i].cardType,
-                              onTapHandler: () {
+                                child: ECardWidget(
+                                  cardType: playerDeck[i].cardType,
+                                  onTapHandler: () {
+                                    context
+                                        .read(eCardProvider.notifier)
+                                        .setPlayerCard(provider.phase + 1, i);
+                                  },
+                                ),
+                              ),
+                            ),
+                        ],
+                      )
+                    else
+                      ElevatedButton(
+                        onPressed: () {
+                          scanNfcTag().then((value) {
+                            var findFlag = false;
+                            for (int i = 0; i < playerDeck.length && !findFlag; i++) {
+                              if (playerDeck[i].cardType == nfcs[value.toString()]) {
+                                // 見つけた
+                                findFlag = true;
+                                print(playerDeck[i].cardType);
+                                print("を選択");
+
+                                nfcs.remove(value.toString());
+
                                 context
                                     .read(eCardProvider.notifier)
                                     .setPlayerCard(provider.phase + 1, i);
-                              },
-                            ),
-                          ),
-                        ),
-                    ],
-                  )
-                else
-                  ElevatedButton(
-                    onPressed: () {
-                      scanNfcTag().then((value) => print(value));
-                    },
-                    child: Text("NFC"),
-                  )
-              ],
-            ),
-          ),
-        if (provider.winFlag != 0)
-          GestureDetector(
-            onTap: () => Navigator.pop(context),
-            child: Center(
-                child: Text(
-              "${provider.winFlag}P 圧倒的 勝利っ・・",
-              style: const TextStyle(fontSize: 48),
-            )),
-          ),
-      ],
-    ));
+                              }
+                            }
+                          });
+                        },
+                        child: Text("NFC"),
+                      )
+                  ],
+                ),
+              ),
+            if (provider.winFlag != 0)
+              GestureDetector(
+                onTap: () => Navigator.pop(context),
+                child: Center(
+                    child: Text(
+                      "${provider.winFlag}P 圧倒的 勝利っ・・",
+                      style: const TextStyle(fontSize: 48),
+                    )),
+              ),
+          ],
+        ));
   }
 }
 
@@ -170,22 +199,22 @@ class ECardWidget extends StatelessWidget {
     Color color = Colors.white;
 
     switch (cardType) {
-      // 奴隷
+    // 奴隷
       case CardType.slave:
         text = "奴隷";
         color = const Color(0xffe57373);
         break;
-      // 市民
+    // 市民
       case CardType.citizen:
         text = "市民";
         color = const Color(0xff73e5e5);
         break;
-      // 皇帝
+    // 皇帝
       case CardType.emperor:
         text = "皇帝";
         color = const Color(0xffe5e573);
         break;
-      // 裏面
+    // 裏面
       default:
         text = "Eカード";
         color = Colors.black;
